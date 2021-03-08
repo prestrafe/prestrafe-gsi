fc4a738c74e17877c6c85dab2f33ee074c7c68f8
Jan Gräfen <223234+jangraefen@users.noreply.github.com>
2021-03-08 14:34:17 +0100

    Start metric server

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/kelseyhightower/envconfig"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"gitlab.com/prestrafe/prestrafe-gsi/server"
 )
 
 type ServerConfig struct {
-	Addr string `default:""`
-	Port int    `default:"8080"`
-	Ttl  int    `default:"15"`
+	Addr       string `default:""`
+	Port       int    `default:"8080"`
+	MetricPort int    `default:"9080"`
+	Ttl        int    `default:"15"`
 }
 
 func main() {
 	config := new(ServerConfig)
 	envconfig.MustProcess("gsi", config)
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		_ = http.ListenAndServe(fmt.Sprintf(":%d", config.MetricPort), nil)
+	}()
 
 	gsiServer := server.New(config.Addr, config.Port, config.Ttl, &server.ToggleTokenFilter{Value: true})
 	if err := gsiServer.Start(); err != nil {
